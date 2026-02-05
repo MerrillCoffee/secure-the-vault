9357e504d67928123d8fa7654bd34c7568a48b61
Merrill <andrewjmarsden98@gmail.com>
2026-02-04 21:54:35 -0500

    Completed assignment

---
+++ index.js
@@ -0,0 +1,15 @@
+let code1;
+code1 = 7+3;
+
+let code2;
+code2 = 5*5+15
+
+let code3;
+code3 = (150%100)-11
+
+let message;
+message = "The vault has been secured. The combination is:"
+
+let codeA = code1 + "-" + code2 + "-" + code3;
+
+let codeB = `${code1}-${code2}-${code3}`;
